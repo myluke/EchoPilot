b70ac96ab4dcb71e99f80c425d3db64586d28d82
Lukin <mylukin@gmail.com>
2024-03-16 11:22:26 +0800

    Add Ethereum signature verification and hashing functions

--- helper/validate.go
+++ helper/validate.go
@@ -1,9 +1,13 @@
 package helper
 
 import (
+	"errors"
+	"fmt"
 	"regexp"
 	"unicode"
 
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
@@ -14,7 +18,8 @@ func IsURL(token string) bool {
 
 // IsObjectID is object id
 func IsObjectID(v string) bool {
-	return primitive.IsValidObjectID(v)
+	_, err := primitive.ObjectIDFromHex(v)
+	return err == nil
 }
 
 // 检测是否为英文
@@ -25,4 +30,32 @@ func IsEnglish(str string) bool {
 		}
 	}
 	return true
+}
+
+// SignHash creates a hash for signing a message.
+func SignHash(data []byte) []byte {
+	return crypto.Keccak256([]byte(fmt.Sprintf("\x19Ethereum Signed Message:\n%d%s", len(data), data)))
+}
+
+// EnsureOwner verifies that the signature corresponds to the given address.
+func EnsureOwner(address, message, signature string) (common.Address, error) {
+	address1 := common.HexToAddress(address)
+	rawSig := common.FromHex(signature)
+
+	if len(rawSig) != 65 { // Ethereum signatures are 65 bytes
+		return common.Address{}, errors.New("bad signature length")
+	}
+
+	rawSig[64] -= 27 // Adjust the recovery ID
+
+	publicKey, err := crypto.SigToPub(SignHash([]byte(message)), rawSig)
+	if err != nil {
+		return common.Address{}, err
+	}
+
+	if owner := crypto.PubkeyToAddress(*publicKey); owner != address1 {
+		return common.Address{}, errors.New("mismatch")
+	}
+
+	return address1, nil
 }
