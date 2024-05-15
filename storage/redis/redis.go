42ba0e697941033edca0190d0dcf23be77804f5d
Lukin <mylukin@gmail.com>
2024-05-15 14:44:06 +0800

    chore: Add AddQueue and RunQueue functions for Redis queue management

--- storage/redis/redis.go
+++ storage/redis/redis.go
@@ -2,11 +2,13 @@ package redis
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"strconv"
 	"strings"
 	"time"
 
+	"github.com/labstack/gommon/log"
 	"github.com/mylukin/EchoPilot/helper"
 	"github.com/redis/go-redis/v9"
 )
@@ -147,4 +149,105 @@ func DayDo(name string) int {
 // MonthDo 每月可执行次数
 func MonthDo(name string) int {
 	return AnyDo(name, 30*24*time.Hour)
+}
+
+// AddQueue
+func AddQueue(queueKey string, i interface{}, delay time.Duration) error {
+	byteData, err := json.Marshal(i)
+	if err != nil {
+		return err
+	}
+
+	return GetRedis().ZAdd(context.Background(), GetCacheKey(queueKey), redis.Z{
+		Score:  float64(time.Now().Add(delay).Unix()),
+		Member: byteData,
+	}).Err()
+}
+
+// RunQueue
+// // 一条一条的处理
+// redis.RunQueue("task_queue", 10, func(data []byte) (interface{}, error) {
+// 	log.Infof("data: %s", string(data))
+// 	return nil, nil
+// })
+
+// // 批量处理
+//
+//	redis.RunQueue("task_queue", 10, func(data []byte) (interface{}, error) {
+//		log.Infof("data: %s", string(data))
+//		return nil, nil
+//	}, func(results []interface{}) error {
+//		// 在这里批量处理上面的返回结果
+//		log.Infof("results: %v", results)
+//		return nil
+//	})
+func RunQueue(queueKey string, batchNum int, callback func([]byte) (interface{}, error), callbacks ...func([]interface{}) error) {
+	queueKey = GetCacheKey(queueKey)
+	cRedis := GetRedis()
+	ticker := time.NewTicker(1 * time.Second)
+	ctx := context.Background()
+
+	for range ticker.C {
+		results := []interface{}{}
+		res, err := cRedis.ZRangeByScoreWithScores(ctx, queueKey, &redis.ZRangeBy{
+			Min:    "-inf",
+			Max:    fmt.Sprintf("%d", time.Now().Unix()),
+			Offset: 0,
+			Count:  int64(batchNum),
+		}).Result()
+
+		// 处理失败，跳过
+		if err != nil {
+			// 队列为空，退出
+			if err == RedisNil {
+				// 队列为空，继续等待下一次查询
+				continue
+			}
+			log.Error(err)
+			continue
+		}
+
+		if len(res) == 0 {
+			// 没有到期任务，继续等待下一次查询
+			continue
+		}
+
+		// log.Infof("res: %v", res)
+
+		members := []interface{}{}
+		for _, rz := range res {
+			// 到了执行时间
+			data := rz.Member.(string)
+
+			// 执行函数
+			r, err := callback([]byte(data))
+			if err != nil {
+				log.Error(err)
+				continue
+			}
+
+			members = append(members, rz.Member)
+
+			if len(callbacks) > 0 {
+				results = append(results, r)
+			}
+		}
+
+		// 移除已成功处理的数据
+		if len(members) > 0 {
+			err := cRedis.ZRem(ctx, queueKey, members...)
+			if err != nil {
+				log.Error("Error removing processed members: ", err)
+			} else {
+				log.Infof("Removed processed members: %v", members)
+			}
+		}
+
+		// 批量操作执行
+		for _, cb := range callbacks {
+			if err := cb(results); err != nil {
+				log.Error(err)
+			}
+		}
+	}
 }
